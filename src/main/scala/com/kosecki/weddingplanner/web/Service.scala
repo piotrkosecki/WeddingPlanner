e9b231e14fa08990306921cab2c6df03c9aede20
fr3ak <piotr.kosecki@gmail.com>
2016-03-25 13:50:57 +0100

    optimized imports

--- src/main/scala/com/kosecki/weddingplanner/web/Service.scala
+++ src/main/scala/com/kosecki/weddingplanner/web/Service.scala
@@ -4,15 +4,15 @@ import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
-import akka.stream.Materializer
-import com.kosecki.weddingplanner.web.DAO.{GuestsDAOInMemory, GuestsDAOPersistent}
-import com.typesafe.config.ConfigFactory
 import akka.pattern._
+import akka.stream.Materializer
 import akka.util.Timeout
+import com.kosecki.weddingplanner.web.DAO.{GuestsDAOInMemory, GuestsDAOPersistent}
 import com.kosecki.weddingplanner.web.Service._
+import com.typesafe.config.ConfigFactory
 
-import scala.concurrent.duration._
 import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration._
 
 
 object Service {
